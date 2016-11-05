29ba5e174d87cd4f2651b319cdee4934b90e371f
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2016-11-05 01:02:51 -0400

    basic sql statements for creating database

---
+++ WildFire/Models/Character.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+namespace WildFire.Models
+{
+    public class Character
+    {
+        public string Name;
+
+        public static void Create(string characterName) {
+            var character = new Character();
+            character.Name = characterName;
+            GlobalVariables.Characters.Add(character);
+        }
+
+        public static List<Character> GetAll() {
+            return GlobalVariables.Characters;
+        }
+    }
+}
---
+++ WildFire/Models/sqlStatements.cs
@@ -0,0 +1,28 @@
+//Make some fresh tables using executescript()
+
+//basic queries that need to be updated for CS language
+/*cur.executescript('''
+DROP TABLE IF EXISTS Artist;
+DROP TABLE IF EXISTS Album;
+DROP TABLE IF EXISTS Track;
+
+CREATE TABLE Artist (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    name    TEXT UNIQUE
+);
+
+
+CREATE TABLE Album (
+    id  INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
+    artist_id  INTEGER,
+    title   TEXT UNIQUE
+);
+
+CREATE TABLE Track (
+    id  INTEGER NOT NULL PRIMARY KEY
+        AUTOINCREMENT UNIQUE,
+    title TEXT  UNIQUE,
+    album_id  INTEGER,
+    len INTEGER, rating INTEGER, count INTEGER
+);
+''') */
