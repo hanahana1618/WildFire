eafbe3426a2929b8834571a504f0aba3e5c4eb57
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2016-11-07 20:56:20 -0500

    characterController done

--- CharacterDB/ForgingAhead/Controllers/CharacterController.cs
+++ CharacterDB/ForgingAhead/Controllers/CharacterController.cs
@@ -24,9 +24,30 @@ namespace ForgingAhead.Controllers {
             return View(model);
         }
 
-        pubic IActionResult GetActive() {
+        public IActionResult GetActive() {
             var model = _context.Characters.Where(e => e.IsActive).ToList();
             return View(model);
+        }
+
+        public IActionResult Details(string name) {
+            //getting one character instead of a collection
+            var model = _context.Characters.FirstOrDefault(e => e.Name == name);
+            return View(model);
+        }
+
+        public IActionResult Update(Character character) {
+            _context.Entry(character).State = EntityState.Modified;
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult Delete(string name) {
+            var original = _context.Characters.FirstOrDefault(e => e.Name == name);
+            if (original != null) {
+                _context.Characters.Remove(original);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
