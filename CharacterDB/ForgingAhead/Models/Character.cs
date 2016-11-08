fc211519643c2c1384a894b8e95e177776db35ff
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2016-11-07 20:57:37 -0500

    going through changes

--- CharacterDB/ForgingAhead/Models/Character.cs
+++ CharacterDB/ForgingAhead/Models/Character.cs
@@ -1,8 +1,9 @@
 using System;
 
 namespace ForgingAhead.Models {
+    //this is the basis of the database
     public class Character {
         public string Name { get; set; }
-        public string id { get; set; }
+        public string ID { get; set; }
     }
 }
