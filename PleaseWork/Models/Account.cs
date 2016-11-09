4e4e89ae052c99975f93e3da9215a437f1052a08
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2016-11-09 04:12:25 -0500

    added a new project that gives me hope

---
+++ PleaseWork/Models/Account.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Yeoman.Models {
+    public class Account {
+        [RequiredAttribute]
+        public string FirstNameAccount { get; set; }
+
+        [RequiredAttribute]
+        public string LastNameAccount { get; set; }
+
+        [RequiredAttribute]
+        public string Password { get; set; }
+
+        [RequiredAttribute]
+        [KeyAttribute]
+        public string Email { get; set; }
+
+        public string Picture { get; set; }
+
+        //creates a many-to-many relationship between both tables
+        public List<Project> Projects { get; set; }
+
+    }
+}
---
+++ PleaseWork/Models/Project.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Yeoman.Models {
+    public class Project {
+        [RequiredAttribute]
+        [KeyAttribute] //makes the field unique
+        public string NameProject { get; set; }
+
+        [RequiredAttribute]
+        public string Description { get; set; }
+
+        [RequiredAttribute]
+        public string Subject { get; set; }
+
+        public string PictureProject { get; set; }
+
+        public int Funding { get; set; }
+
+        public string Skills { get; set; }
+    
+        [RequiredAttribute]
+        public int PercentageAchieved { get; set; }
+    }
+}
