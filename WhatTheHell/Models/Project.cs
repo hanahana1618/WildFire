5dbf4aa28e572111f94301b8586a3086090ee507
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2016-11-09 16:23:15 -0500

    working on controllers

---
+++ WhatTheHell/Models/Project.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models {
+    public class Project {
+        [RequiredAttribute]
+        [KeyAttribute] //makes the field unique
+        public virtual string NameProject { get; set; }
+
+        [RequiredAttribute]
+        public virtual string Description { get; set; }
+
+        [RequiredAttribute]
+        public virtual string Subject { get; set; }
+
+        public virtual string PictureProject { get; set; }
+
+        public virtual int Funding { get; set; }
+
+        public virtual string Skills { get; set; }
+    
+        [RequiredAttribute]
+        public virtual int PercentageAchieved { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
---
+++ WhatTheHell/Models/User.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models {
+    public class User {
+        [RequiredAttribute]
+        public virtual string FirstNameAccount { get; set; }
+
+        [RequiredAttribute]
+        public virtual string LastNameAccount { get; set; }
+
+        [RequiredAttribute]
+        public virtual string Password { get; set; }
+
+        [RequiredAttribute]
+        [KeyAttribute]
+        public virtual string Email { get; set; }
+
+        public string Picture { get; set; }
+
+        //creates a one-to-many relationship between both tables
+        public virtual List<Project> Projects { get; set; }
+        //public List<Project> Projects { get; set; } // = new List<Project>();
+
+    }
+}
