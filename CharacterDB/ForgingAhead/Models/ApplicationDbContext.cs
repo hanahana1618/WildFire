5b8a9cb20ef72bc86a03ed5465d82b2526c2b125
Ivette Castro ivetto@Ivetto_Mac.local <hanahana1.618@hotmail.es>
2016-11-09 16:04:10 -0500

    something

--- CharacterDB/ForgingAhead/Models/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using Microsoft.EntityFrameworkCore;
-
-namespace ForgingAhead.Models {
-    public class ApplicationDbContext : DbContext {
-        public DbSet<Character> Characters { get; set; }
-
-    }
-}
